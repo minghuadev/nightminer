02a2a65883d7d99f7aad143f053c2816c09af454
minghuadev <minghuadev@users.noreply.github.com>
2021-06-26 17:19:41 -0700

    manually apply the patch from lucas556 for python3

--- nightminer.py
+++ nightminer.py
@@ -38,7 +38,11 @@
 #     Scrypt Algorithm                - http://www.tarsnap.com/scrypt/scrypt.pdf
 #     Scrypt Implementation     - https://code.google.com/p/scrypt/source/browse/trunk/lib/crypto/crypto_scrypt-ref.c
 
-import base64, binascii, json, hashlib, hmac, math, socket, struct, sys, threading, time, urlparse
+import base64, binascii, json, hashlib, hmac, math, socket, struct, sys, threading, time, urllib.parse
+import codecs
+
+encode_hex = codecs.getencoder("hex_codec")
+decode_hex = codecs.getdecoder("hex_codec")
 
 # DayMiner (ah-ah-ah), fighter of the...
 USER_AGENT = "NightMiner"
@@ -53,7 +57,7 @@ ALGORITHM_SHA256D         = 'sha256d'
 
 ALGORITHMS = [ ALGORITHM_SCRYPT, ALGORITHM_SHA256D ]
 
-FPGA = True 
+FPGA = False
 
 
 # Verbosity and log level
@@ -115,7 +119,7 @@ def swap_endian_words(hex_words):
 
     message = unhexlify(hex_words)
     if len(message) % 4 != 0: raise ValueError('Must be 4-byte word aligned')
-    return ''.join([ message[4 * i: 4 * i + 4][::-1] for i in range(0, len(message) // 4) ])
+    return b''.join([ message[4 * i: 4 * i + 4][::-1] for i in range(0, len(message) // 4) ])
 
 
 def human_readable_hashrate(hashrate):
@@ -154,15 +158,16 @@ def scrypt(password, salt, N, r, p, dkLen):
     def array_overwrite(source, source_start, dest, dest_start, length):
         '''Overwrites the dest array with the source array.'''
 
-        for i in xrange(0, length):
+        for i in range(0, length):
             dest[dest_start + i] = source[source_start + i]
 
 
     def blockxor(source, source_start, dest, dest_start, length):
         '''Performs xor on arrays source and dest, storing the result back in dest.'''
 
-        for i in xrange(0, length):
-            dest[dest_start + i] = chr(ord(dest[dest_start + i]) ^ ord(source[source_start + i]))
+        for i in range(0, length):
+            #dest[dest_start + i] = chr(ord(dest[dest_start + i]) ^ ord(source[source_start + i]))
+            dest[dest_start + i] = dest[dest_start + i] ^ source[source_start + i]
 
 
     def pbkdf2(passphrase, salt, count, dkLen, prf):
@@ -179,7 +184,7 @@ def scrypt(password, salt, N, r, p, dkLen):
             # Not used for scrpyt-based coins, could be removed, but part of a more general solution
             if count > 1:
                 U = [ c for c in U ]
-                for i in xrange(2, 1 + count):
+                for i in range(2, 1 + count):
                     blockxor(prf(passphrase, ''.join(U)), 0, U, 0, len(U))
                 U = ''.join(U)
 
@@ -199,13 +204,14 @@ def scrypt(password, salt, N, r, p, dkLen):
             blocks.append(block)
             size += len(block)
 
-        return ''.join(blocks)[:dkLen]
+        return b''.join(blocks)[:dkLen]
 
     def integerify(B, Bi, r):
         '''"A bijective function from ({0, 1} ** k) to {0, ..., (2 ** k) - 1".'''
 
         Bi += (2 * r - 1) * 64
-        n    = ord(B[Bi]) | (ord(B[Bi + 1]) << 8) | (ord(B[Bi + 2]) << 16) | (ord(B[Bi + 3]) << 24)
+        #n    = ord(B[Bi]) | (ord(B[Bi + 1]) << 8) | (ord(B[Bi + 2]) << 16) | (ord(B[Bi + 3]) << 24)
+        n    = B[Bi] | (B[Bi + 1] << 8) | (B[Bi + 2] << 16) | (B[Bi + 3] << 24)
         return n
 
 
@@ -227,11 +233,12 @@ def scrypt(password, salt, N, r, p, dkLen):
         '''Salsa 20/8 stream cypher; Used by BlockMix. See http://en.wikipedia.org/wiki/Salsa20'''
 
         # Convert the character array into an int32 array
-        B32 = [ make_int32((ord(B[i * 4]) | (ord(B[i * 4 + 1]) << 8) | (ord(B[i * 4 + 2]) << 16) | (ord(B[i * 4 + 3]) << 24))) for i in xrange(0, 16) ]
+        #B32 = [ make_int32((ord(B[i * 4]) | (ord(B[i * 4 + 1]) << 8) | (ord(B[i * 4 + 2]) << 16) | (ord(B[i * 4 + 3]) << 24))) for i in range(0, 16) ]
+        B32 = [ make_int32((B[i * 4] | (B[i * 4 + 1] << 8) | (B[i * 4 + 2] << 16) | (B[i * 4 + 3] << 24))) for i in range(0, 16) ]
         x = [ i for i in B32 ]
 
         # Salsa... Time to dance.
-        for i in xrange(8, 0, -2):
+        for i in range(8, 0, -2):
             R(x, 4, 0, 12, 7);     R(x, 8, 4, 0, 9);        R(x, 12, 8, 4, 13);     R(x, 0, 12, 8, 18)
             R(x, 9, 5, 1, 7);        R(x, 13, 9, 5, 9);     R(x, 1, 13, 9, 13);     R(x, 5, 1, 13, 18)
             R(x, 14, 10, 6, 7);    R(x, 2, 14, 10, 9);    R(x, 6, 2, 14, 13);     R(x, 10, 6, 2, 18)
@@ -242,31 +249,31 @@ def scrypt(password, salt, N, r, p, dkLen):
             R(x, 12, 15, 14, 7); R(x, 13, 12, 15, 9); R(x, 14, 13, 12, 13); R(x, 15, 14, 13, 18)
 
         # Coerce into nice happy 32-bit integers
-        B32 = [ make_int32(x[i] + B32[i]) for i in xrange(0, 16) ]
+        B32 = [ make_int32(x[i] + B32[i]) for i in range(0, 16) ]
 
         # Convert back to bytes
-        for i in xrange(0, 16):
-            B[i * 4 + 0] = chr((B32[i] >> 0) & 0xff)
-            B[i * 4 + 1] = chr((B32[i] >> 8) & 0xff)
-            B[i * 4 + 2] = chr((B32[i] >> 16) & 0xff)
-            B[i * 4 + 3] = chr((B32[i] >> 24) & 0xff)
+        for i in range(0, 16):
+            B[i * 4 + 0] = (B32[i] >> 0) & 0xff
+            B[i * 4 + 1] = (B32[i] >> 8) & 0xff
+            B[i * 4 + 2] = (B32[i] >> 16) & 0xff
+            B[i * 4 + 3] = (B32[i] >> 24) & 0xff
 
 
     def blockmix_salsa8(BY, Bi, Yi, r):
         '''Blockmix; Used by SMix.'''
 
         start = Bi + (2 * r - 1) * 64
-        X = [ BY[i] for i in xrange(start, start + 64) ]                                     # BlockMix - 1
+        X = [ BY[i] for i in range(start, start + 64) ]                                     # BlockMix - 1
 
-        for i in xrange(0, 2 * r):                                                                                 # BlockMix - 2
+        for i in range(0, 2 * r):                                                                                 # BlockMix - 2
             blockxor(BY, i * 64, X, 0, 64)                                                                     # BlockMix - 3(inner)
             salsa20_8(X)                                                                                                         # BlockMix - 3(outer)
             array_overwrite(X, 0, BY, Yi + (i * 64), 64)                                         # BlockMix - 4
 
-        for i in xrange(0, r):                                                                                         # BlockMix - 6 (and below)
+        for i in range(0, r):                                                                                         # BlockMix - 6 (and below)
             array_overwrite(BY, Yi + (i * 2) * 64, BY, Bi + (i * 64), 64)
 
-        for i in xrange(0, r):
+        for i in range(0, r):
             array_overwrite(BY, Yi + (i * 2 + 1) * 64, BY, Bi + (i + r) * 64, 64)
 
 
@@ -275,11 +282,11 @@ def scrypt(password, salt, N, r, p, dkLen):
 
         array_overwrite(B, Bi, X, 0, 128 * r)                             # ROMix - 1
 
-        for i in xrange(0, N):                                                            # ROMix - 2
+        for i in range(0, N):                                                            # ROMix - 2
             array_overwrite(X, 0, V, i * (128 * r), 128 * r)    # ROMix - 3
             blockmix_salsa8(X, 0, 128 * r, r)                                 # ROMix - 4
 
-        for i in xrange(0, N):                                                            # ROMix - 6
+        for i in range(0, N):                                                            # ROMix - 6
             j = integerify(X, 0, r) & (N - 1)                                 # ROMix - 7
             blockxor(V, j * (128 * r), X, 0, 128 * r)                 # ROMix - 8(inner)
             blockmix_salsa8(X, 0, 128 * r, r)                                 # ROMix - 9(outer)
@@ -298,10 +305,10 @@ def scrypt(password, salt, N, r, p, dkLen):
     XY = [ chr(0) ] * (256 * r)
     V    = [ chr(0) ] * (128 * r * N)
 
-    for i in xrange(0, p):
+    for i in range(0, p):
         smix(B, i * 128 * r, r, N, V, XY)
 
-    return pbkdf2(password, ''.join(B), 1, dkLen, prf)
+    return pbkdf2(password, bytes(B), 1, dkLen, prf)
 
 
 SCRYPT_LIBRARY = None
@@ -330,10 +337,10 @@ def set_scrypt_library(library = SCRYPT_LIBRARY_AUTO):
     elif library == SCRYPT_LIBRARY_AUTO:
         try:
             set_scrypt_library(SCRYPT_LIBRARY_LTC)
-        except Exception, e:
+        except Exception as e:
             try:
                 set_scrypt_library(SCRYPT_LIBRARY_SCRYPT)
-            except Exception, e:
+            except Exception as e:
                 set_scrypt_library(SCRYPT_LIBRARY_PYTHON)
 
     else:
@@ -387,7 +394,7 @@ class Job(object):
         self.ser = Serial('COM7', 115200, timeout=2)
         resp = self.ser.read(1000)
         while len( resp ) > 0:
-            print ".",
+            print(".", end="")
             resp = self.ser.read(1000)
 
     # Accessors
@@ -449,7 +456,7 @@ class Job(object):
         t0 = time.time()
 
         # @TODO: test for extranounce != 0... Do I reverse it or not?
-        for extranounce2 in xrange(0, 0x7fffffff):
+        for extranounce2 in range(0, 0x7fffffff):
 
             # Must be unique for any given job id, according to http://mining.bitcoin.cz/stratum-mining/ but never seems enforced?
             extranounce2_bin = struct.pack('<I', extranounce2)
@@ -465,27 +472,27 @@ class Job(object):
                 nounce_bin = struct.pack('<I', nounce_start)
                 b = header_prefix_bin + nounce_bin
                 o = ''
-                print '$$b', len(b)
-                print '$$n', nounce_bin.encode('hex')
-                print '$$t', self.target 
+                print('$$b', len(b))
+                print('$$n', encode_hex(nounce_bin)) #nounce_bin.encode('hex'))
+                print('$$t', self.target)
                 #for p in range( 0, len(b), 4 ):
                 #    o += b[p+3] + b[p+2] + b[p+1] + b[p]
                 payload = self.target.decode('hex')[::-1] + b 
-                print '$$p', len(payload)
-                print '@@', payload.encode('hex')
+                print('$$p', len(payload))
+                print('@@', encode_hex(payload)) #payload.encode('hex'))
                 self.ser.write(payload[::-1])
 
                 while True:
-                    print '#'
+                    print('#')
                     if self._done:
                         self._dt += (time.time() - t0)
                         raise StopIteration()
                     nounce_read = self.ser.read(8)
                     
                     if len(nounce_read) == 8:
-                        v = nounce_read.encode('hex')
-                        nounce_bin = v[:8].decode('hex')
-                        print "\nCluster/Core: %s/%s found %s" % ( v[14:16], v[12:14], v[:8] )
+                        v = encode_hex(nounce_read) #nounce_read.encode('hex')
+                        nounce_bin = decode_hex(v[:8]) # v[:8].decode('hex')
+                        print("\nCluster/Core: %s/%s found %s" % ( v[14:16], v[12:14], v[:8] ))
                         result = dict(
                             job_id = self.id,
                             extranounce2 = hexlify(extranounce2_bin),
@@ -501,7 +508,7 @@ class Job(object):
                     self._hash_count += 1
                 return
 
-            for nounce in xrange(nounce_start, 0x7fffffff, nounce_stride):
+            for nounce in range(nounce_start, 0x7fffffff, nounce_stride):
                 # This job has been asked to stop
                 if self._done:
                     self._dt += (time.time() - t0)
@@ -509,10 +516,14 @@ class Job(object):
 
                 # Proof-of-work attempt
                 nounce_bin = struct.pack('<I', nounce)
-                pow = self.proof_of_work(header_prefix_bin + nounce_bin)[::-1].encode('hex')
+                #pow = self.proof_of_work(header_prefix_bin + nounce_bin)[::-1].encode('hex')
+                pow = encode_hex(self.proof_of_work(header_prefix_bin + nounce_bin)[::-1])[0].decode()
+                tgt = self.target
+
+                pow_int, target_int = int(pow, 16), int(tgt, 16)
 
                 # Did we reach or exceed our target?
-                if pow <= self.target:
+                if pow_int <= target_int:
                     result = dict(
                         job_id = self.id,
                         extranounce2 = hexlify(extranounce2_bin),
@@ -687,7 +698,7 @@ class SimpleJsonRpcClient(object):
                 (line, data) = data.split('\n', 1)
             else:
                 chunk = self._socket.recv(1024)
-                data += chunk
+                data += chunk.decode()
                 continue
 
             log('JSON-RPC Server > ' + line, LEVEL_PROTOCOL)
@@ -695,7 +706,7 @@ class SimpleJsonRpcClient(object):
             # Parse the JSON
             try:
                 reply = json.loads(line)
-            except Exception, e:
+            except Exception as e:
                 log("JSON-RPC Error: Failed to parse JSON %r (skipping)" % line, LEVEL_ERROR)
                 continue
 
@@ -705,7 +716,7 @@ class SimpleJsonRpcClient(object):
                     if 'id' in reply and reply['id'] in self._requests:
                         request = self._requests[reply['id']]
                     self.handle_reply(request = request, reply = reply)
-            except self.RequestReplyWarning, e:
+            except self.RequestReplyWarning as e:
                 output = e.message
                 if e.request:
                     output += '\n    ' + e.request
@@ -729,7 +740,7 @@ class SimpleJsonRpcClient(object):
         with self._lock:
             self._requests[self._message_id] = request
             self._message_id += 1
-            self._socket.send(message + '\n')
+            self._socket.send((message + '\n').encode())
 
         log('JSON-RPC Server < ' + message, LEVEL_PROTOCOL)
 
@@ -873,7 +884,7 @@ class Miner(SimpleJsonRpcClient):
                     self.send(method = 'mining.submit', params = params)
                     log("Found share: " + str(params), LEVEL_INFO)
                 log("Hashrate: %s" % human_readable_hashrate(job.hashrate), LEVEL_INFO)
-            except Exception, e:
+            except Exception as e:
                 log("ERROR: %s" % e, LEVEL_ERROR)
 
         thread = threading.Thread(target = run, args = (self._job, ))
@@ -885,7 +896,7 @@ class Miner(SimpleJsonRpcClient):
         '''Begins the miner. This method does not return.'''
 
         # Figure out the hostname and port
-        url = urlparse.urlparse(self.url)
+        url = urllib.parse.urlparse(self.url)
         hostname = url.hostname or ''
         port = url.port or 9333
 
@@ -985,14 +996,14 @@ if __name__ == '__main__':
         else:
             try:
                 (username, password) = options.userpass.split(':')
-            except Exception, e:
+            except Exception as e:
                 message = 'Could not parse username:password for -O/--userpass'
 
     # Was there an issue? Show the help screen and exit.
     if message:
         parser.print_help()
-        print
-        print message
+        print("")
+        print(message)
         sys.exit(1)
 
     # Set the logging level
